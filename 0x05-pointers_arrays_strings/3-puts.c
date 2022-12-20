513735e4507dcd638d497bc1b127ed38011541c2
Bdjiedjom <bdjiedjom31@gmail.com>
2022-12-20 02:16:52 -0800

    printting strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _puts - print the string follow by new line
+ * @str: character pointer to a variable to be print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
