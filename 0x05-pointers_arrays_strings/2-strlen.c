f831bb1577790a88f6704b42030d5ddf930c8b9c
Bdjiedjom <bdjiedjom31@gmail.com>
2022-12-20 02:05:39 -0800

    charater length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string give as parametre
+ * @s: character pointer that point to the string
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
