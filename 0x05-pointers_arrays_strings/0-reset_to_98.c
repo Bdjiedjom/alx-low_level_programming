a999f411178b2585f748889cbe51b56a0a37438e
Bdjiedjom <bdjiedjom31@gmail.com>
2022-12-20 01:11:39 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+
+/**
+ * reset_to_98 - update the value of n to 98
+ * @n : integer pointer to a variable to be update
+ * Description: updating the value of n to 98
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
