a2957e3fc50a3db79b79077243be53d1c9d70052
Bdjiedjom <bdjiedjom31@gmail.com>
2022-12-22 15:37:07 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print of 10 first number begin from 0
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf ("%d",n);
+	}
+	printf("\n");
+	return (0);
+}
