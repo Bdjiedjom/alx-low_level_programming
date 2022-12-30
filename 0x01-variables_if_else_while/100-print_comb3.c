8830cdaf94b94c007474250c3c17bafca53c8b36
Bdjiedjom <bdjiedjom31@gmail.com>
2022-12-29 22:05:21 -0800

    digits combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print of all possible combination of digits
+ * Return: always (0) Sucess
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0' ; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (j != '9' || i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+int _putchar(int);
+
+/**
+ * main - entry point
+ * Description: print of _putchar
+ * Return: always (0) Succes
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+	
