d476f100a81da2fce33b3e123ca6f3b6f1b9a18e
Bdjiedjom <bdjiedjom31@gmail.com>
2023-01-02 08:55:57 -0800

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,24 +9,24 @@ void times_table(void)
 {
 	int i, j, tens, unit, prod;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			prod = i * j;
 			tens = prod / 10;
 			unit = prod % 10;
-			if (j != '9')
+			if (j != 9)
 			{
-				_putchar(tens);
-				_putchar(unit);
+				_putchar(tens + '0');
+				_putchar(unit + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar(tens);
-				_putchar(unit);
+				_putchar(tens + '0');
+				_putchar(unit + '0');
 				_putchar('\n');
 			}
 		}
