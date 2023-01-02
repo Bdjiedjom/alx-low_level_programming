57087284865ad1c7eb548a35c0bda3754ec76328
Bdjiedjom <bdjiedjom31@gmail.com>
2023-01-02 05:07:47 -0800

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,37 +7,32 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, hrs_max;
 
 	for (i = '0'; i <= '2'; i++)
 	{
-		for (j = '0'; j <= '3'; j++)
+		if (i == '2')
+		{
+			hrs_max = '4'
+		}
+		else
+		{
+			hrs_max = '9'
+		}
+		for (j = '0'; j <= hrs_max; j++)
 		{
 			for (k = '0'; k <= '5'; k++)
 			{
 				l = '0';
 				while (l <= '9')
 				{
-					if (j == '3' && l == '9')
-					{
-						_putchar(i);
-                                                _putchar(j);
-                                                _putchar(':');
-                                                _putchar(k);
-                                                _putchar(l);
-                                                _putchar('\n');
-						break;
-					}
-					else
-					{
-						_putchar(i);
-						_putchar(j);
-						_putchar(':');
-						_putchar(k);
-						_putchar(l);
-						_putchar('\n');
-						l++;
-					}
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+					l++;
 				}
 			}
 		}
