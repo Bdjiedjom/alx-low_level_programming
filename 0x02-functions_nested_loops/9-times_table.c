f8356c2431021b33fc1ab5390c06e3b51b629eda
Bdjiedjom <bdjiedjom31@gmail.com>
2023-01-02 08:50:24 -0800

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table, starting with 0
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, tens, unit, prod;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			prod = i * j;
+			tens = prod / 10;
+			unit = prod % 10;
+			if (j != '9')
+			{
+				_putchar(tens);
+				_putchar(unit);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(tens);
+				_putchar(unit);
+				_putchar('\n');
+			}
+		}
+	}
+}
